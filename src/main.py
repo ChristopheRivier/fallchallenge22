df88c8eaaa7ee41332adaf79f1889f8ee0e4cf16
Marau <chrivier@gmail.com>
2022-12-18 21:33:27 +0100

    suppress used file

--- src/main.py
+++ src/main.py
@@ -44,11 +44,4 @@ while True:
     game = Game(width,height,my_map,my_robot,en_robot,my_matter,opp_matter)
 
     game.calcul_action()
-    #print(my_map, file=sys.stderr, flush=True)
-    #print(my_map[0][0], file=sys.stderr, flush=True)
-    #for i in my_robot:
-    #    print(i, file=sys.stderr, flush=True)
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
